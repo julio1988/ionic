2232f650ee20c192afe981851f5486ec7f4a375f
julio1988 <juliomendonca10@gmail.com>
2018-01-23 22:45:48 -0200

    v

---
+++ src/pages/profile/profle.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'profile',
+  templateUrl: 'profile.html'
+})
+export class ProfilePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+}
